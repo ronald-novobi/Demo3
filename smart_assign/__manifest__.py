f0dd3a8b3d2188fd6dbd91c9b212d9edd1b3a9d4
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2021-12-20 18:52:32 -0500

    Add files via upload

---
+++ smart_assign/__manifest__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+{
+    'name': "Smart Assign",
+    'version': "14.0",
+    'author': "Ronald",
+    'sequence': 1,
+    'summary': """""",
+    'depends': ['stock'],
+    'installable': True,
+}
---
+++ smart_assign/models/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+from . import stock_move_assign
+
+
