2c168aeb75893600f6107a974e48d339b33a4476
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2021-12-20 20:06:46 -0500

    Update __init__.py

--- smart_assign/models/__init__.py
+++ smart_assign/models/__init__.py
@@ -2,6 +2,3 @@
 # Part of CAPTIVEA. Odoo 12 EE.
 
 from . import stock_move_assign
-from odoo.addons.stock.models import stock_move
-stock_move.StockMove._action_assign = stock_move_assign._action_assign
-
