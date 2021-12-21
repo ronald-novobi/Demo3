cd77c6a274b4dc042941110e6e01984b3ef4eaba
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2021-12-21 14:42:28 -0500

    Update stock_move_assign.py

--- smart_assign/models/stock_move_assign.py
+++ smart_assign/models/stock_move_assign.py
@@ -24,8 +24,9 @@ class SmartMove(models.Model):
     _inherit = "stock.move"
     _description = "Stock Move"
     _order = 'sequence, id'
-    raise UserError(_('test'))
+    
     def _action_assign(self):
+        raise UserError(_('test'))
         """ Reserve stock moves by creating their stock move lines. A stock move is
         considered reserved once the sum of `product_qty` for all its move lines is
         equal to its `product_qty`. If it is less, the stock move is considered
