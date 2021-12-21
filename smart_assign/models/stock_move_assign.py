dc907b302d754af846a0a422ddeb627714a880f8
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2021-12-20 19:34:54 -0500

    Update stock_move_assign.py

--- smart_assign/models/stock_move_assign.py
+++ smart_assign/models/stock_move_assign.py
@@ -14,7 +14,7 @@ from odoo.osv import expression
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 from odoo.tools.misc import clean_context, OrderedSet
 PROCUREMENT_PRIORITIES = [('0', 'Normal'), ('1', 'Urgent')]
-from odoo.movels import stock_move
+from odoo.models import stock_move
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
