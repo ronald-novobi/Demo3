a7202e75106b58a2d590e086f19c9264eb958e3a
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2022-01-11 16:25:22 -0500

    Add files via upload

---
+++ form_chatter_resize/__manifest__.py
@@ -0,0 +1,12 @@
+
+
+{
+    'name': "Chatter Resize",
+    'version': "14.0",
+    'author': "Novobi",
+    'sequence': 1,
+    'summary': """""",
+    'depends': [],
+    'data': ['data/assets_chatter_inherit.xml'],
+    'installable': True,
+}
